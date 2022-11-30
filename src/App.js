cdb0a5155162ac01cea433ebd1173fa187ce5b38
顾念\51786 <gunian1011@163.com>
2022-11-30 20:53:54 +0800

    更新路由

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Layout'
 import Login from './pages/Login'
 function App() {
@@ -9,12 +9,16 @@ function App() {
         <Link to="/home">首页</Link> */}
 
         {/* 配置路由的规则 */}
-        <Switch>
-          <Route path="/home" components={Home}></Route>
-          <Route path="/Login" components={Login}></Route>
+        {/* 注意：
+                新版的Switch 更改为Routes
+                components 更改为 element
+        */}
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           {/* 配置404组件 */}
           {/* <Route components={404}></Route> */}
-        </Switch>
+        </Routes>
       </div>
     </Router>
   )
